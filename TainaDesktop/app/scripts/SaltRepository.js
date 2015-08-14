e5eff8dbfc1e2c337fec92758f2ccfe0bbe596f9
Ilya Siganov <ilya.blan4@gmail.com>
2015-08-14 17:34:12 +0600

    add repository for salt and masterKey, add service for crypt

---
+++ TainaDesktop/app/scripts/SaltRepository.js
@@ -0,0 +1,33 @@
+'use strict';
+
+window.SaltRepository = (function() {
+  const Promise = require('bluebird');
+  let module = {};
+
+  module.create = function(cryptoService) {
+    if (!cryptoService) {
+      throw Error('missing cryptoService');
+    }
+
+    let SaltRepository = {};
+    let storage = localStorage;
+    const SALT_KEY = '_taina_salt';
+
+    SaltRepository.findOrCreate = function() {
+      let salt = storage.getItem(SALT_KEY);
+
+      if (!salt) {
+        return cryptoService.generateSalt().then(function(salt) {
+          storage.setItem(SALT_KEY, salt);
+          return salt;
+        });
+      } else {
+        return Promise.resolve(salt);
+      }
+    }
+
+    return SaltRepository;
+  }
+
+  return module;
+})();
--- TainaDesktop/app/scripts/index.js
+++ TainaDesktop/app/scripts/index.js
@@ -1,1 +1,9 @@
-console.log('Hello world');
+'use strict';
+
+let cryptoService = CryptoService.create();
+let saltRepository = SaltRepository.create(cryptoService);
+let masterKeyRepository = MasterKeyRepository.create(saltRepository, cryptoService);
+
+masterKeyRepository.saveKey('qwerty').then(function(key) {
+  console.log(key.toString('hex'));
+});
