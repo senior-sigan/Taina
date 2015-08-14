7e1638877937a676f07414e1e20b78a8a5250594
Ilya Siganov <ilya.blan4@gmail.com>
2015-08-15 00:14:44 +0600

    make all crypt functions to return data in hex format

--- TainaDesktop/app/scripts/CryptoService.js
+++ TainaDesktop/app/scripts/CryptoService.js
@@ -4,18 +4,48 @@ window.CryptoService = (function() {
   const Promise = require('bluebird');
   const Crypto = Promise.promisifyAll(require('crypto'));
   const HASH_ALGORITHM = 'sha1';
+  const ENCRYPTION_ALGORITHM = 'aes-256-cbc';
   const SALT_SIZE = 256;
+  const IV_SIZE = 16;
   let module = {};
 
   module.create = function() {
     let CryptoService = {};
 
-    CryptoService.generateHashFromPassword = function(password, salt) {
-      return Crypto.pbkdf2Async(password, salt, 4096, 32, HASH_ALGORITHM);
+    CryptoService.generateHashFromPassword = function(password, hSalt) {
+      let bSalt = new Buffer(hSalt, 'hex');
+      return Crypto.pbkdf2Async(password, bSalt, 4096, 32, HASH_ALGORITHM).then(function(hash) {
+        return hash.toString('hex');
+      });
     };
 
     CryptoService.generateSalt = function() {
-      return Crypto.randomBytesAsync(SALT_SIZE);
+      return Crypto.randomBytesAsync(SALT_SIZE).then(function(salt) {
+        return salt.toString('hex');
+      });
+    };
+
+    CryptoService.encrypt = function(data, hKey) {
+      let bKey = new Buffer(hKey, 'hex');
+      return Crypto.randomBytesAsync(IV_SIZE).then(function(iv) {
+        let cipher = Crypto.createCipheriv(ENCRYPTION_ALGORITHM, bKey, iv);
+        let enc = cipher.update(data, 'utf8', 'hex') + cipher.final('hex');
+
+        return {
+          data: enc,
+          iv: iv.toString('hex')
+        };
+      });
+    };
+
+    CryptoService.decrypt = function(data, hKey, hIv) {
+      let bKey = new Buffer(hKey, 'hex');
+      let bIv = new Buffer(hIv, 'hex');
+
+      let decipher = Crypto.createDecipheriv(ENCRYPTION_ALGORITHM, bKey, bIv);
+      let dec = decipher.update(data, 'hex', 'utf8') + decipher.final('utf8');
+
+      return Promise.resolve(dec);
     };
 
     return CryptoService;
--- TainaDesktop/app/scripts/index.js
+++ TainaDesktop/app/scripts/index.js
@@ -4,6 +4,14 @@ let cryptoService = CryptoService.create();
 let saltRepository = SaltRepository.create(cryptoService);
 let masterKeyRepository = MasterKeyRepository.create(saltRepository, cryptoService);
 
-masterKeyRepository.saveKey('qwerty').then(function(key) {
-  console.log(key.toString('hex'));
+let key = null;
+
+masterKeyRepository.saveKey('qwerty').then(function(_key) {
+  key = _key;
+  return cryptoService.encrypt('Hello World!!', key);
+}).then(function(dataWithIV) {
+  console.log(dataWithIV);
+  return cryptoService.decrypt(dataWithIV.data, key, dataWithIV.iv);
+}).then(function(openText) {
+  console.log(openText);
 });
