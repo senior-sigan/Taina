c00de9689cf08d237e4fd9d0170f4d6d67824bbc
Ilya Siganov <ilya.blan4@gmail.com>
2015-08-20 00:25:56 +0600

    add repository, update services for masterKey, add main application class

---
+++ TainaDesktop/app/scripts/Taina.js
@@ -0,0 +1,18 @@
+'use strict';
+
+window.Taina = (function() {
+  const Promise = require('bluebird');
+  const module = {};
+
+  module.create = function() {
+    const Taina = {};
+
+    Taina.getNotes = function() {
+
+    };
+
+    return Taina;
+  };
+
+  return module;
+})();
