a9d39cff93af11e06a57cd30556e651e64310019
Ilya Siganov <ilya.blan4@gmail.com>
2015-08-20 11:56:11 +0600

    fix typos, add funtion to encrypt note and show

--- TainaDesktop/app/scripts/index.js
+++ TainaDesktop/app/scripts/index.js
@@ -2,26 +2,32 @@
 
 const winston = require('winston');
 const cryptoAdapter = CryptoAdapter.create();
-const saltRepository = SaltRepository.create(cryptoService);
-const masterKeyRepository = MasterKeyRepository.create(saltRepository, cryptoService);
+const saltRepository = SaltRepository.create(cryptoAdapter);
+const masterKeyRepository = MasterKeyRepository.create(saltRepository, cryptoAdapter);
 const cryptoService = CryptoService.create(masterKeyRepository, cryptoAdapter);
 const db = DB.create();
 const taina = Taina.create(cryptoService, db);
 
+
+// TEST
 let key = null;
-masterKeyRepository.getKey().then(function(key) {
-  winston.info('Key loaded', key);
-  return key;
+winston.info('>>> START TEST');
+masterKeyRepository.getKey().then(function(_key) {
+  winston.info('Key loaded', _key);
+  return _key;
 }).catch(function() {
   winston.info('Creating new key');
   return masterKeyRepository.saveKey('qwerty').then(function(_key) {
-    key = _key;
-    winston.info('New key created', key);
-    return cryptoService.encrypt('Hello World!!', key);
+    winston.info('New key created', _key);
+    return _key;
   });
+}).then(function(_key) {
+  key = _key;
+  return cryptoAdapter.encrypt('Hello World!!', key);
 }).then(function(dataWithIV) {
   winston.info(dataWithIV);
-  return cryptoService.decrypt(dataWithIV.data, key, dataWithIV.iv);
+  return cryptoAdapter.decrypt(dataWithIV.data, dataWithIV.iv, key);
 }).then(function(openText) {
   winston.info(openText);
+  winston.info('<<< TEST COMPLETE');
 });
