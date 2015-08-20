4bea10b5dbcd8c9d7c61e5a21587ad0d3223477f
Ilya Siganov <ilya.blan4@gmail.com>
2015-08-20 10:17:22 +0600

    add cryptoService

--- TainaDesktop/app/scripts/Taina.js
+++ TainaDesktop/app/scripts/Taina.js
@@ -4,12 +4,23 @@ window.Taina = (function() {
   const Promise = require('bluebird');
   const module = {};
 
-  module.create = function() {
+  module.create = function(cryptoService, db) {
     const Taina = {};
 
     Taina.getNotes = function() {
-
+      return db.getAllNotes();
     };
+
+    Taina.addNote = function(title, body) {
+      return cryptoService.encrypt(body).then(function(encryptedBody) {
+        return db.addNote({
+          title: title,
+          body: encryptedBody
+        });
+      });
+    };
+
+    Taina.showNote = function(id) {};
 
     return Taina;
   };
