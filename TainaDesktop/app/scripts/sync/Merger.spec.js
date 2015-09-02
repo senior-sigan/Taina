734eea54f74ad9cb241cc2074906c3b204e72dfe
Ilya Siganov <ilya.blan4@gmail.com>
2015-09-02 14:33:26 +0600

    rm old test

--- TainaDesktop/app/scripts/sync/Merger.spec.js
+++ TainaDesktop/app/scripts/sync/Merger.spec.js
@@ -1,12 +1,110 @@
-import {merge} from './Merger';
+import Merger from './Merger';
 import {expect} from 'chai';
+import Random from '../helpers/Random';
+const merger = Merger.create(Random);
 
 describe('Merger', () => {
   it('should merge empty data', () => {
     const localData = [];
     const remoteData = [];
-    const data = merge(remoteData, localData);
+    const data = merger.merge(remoteData, localData);
     expect(data.localData).to.eql([]);
     expect(data.remoteData).to.eql([]);
+  });
+
+  it('should merge simple states', () => {
+    const remoteData = [{
+      _id: '1r',
+      revision: '1_a',
+      revisions: [{
+        revision: '1_a',
+        data: {'hello': 'world'},
+      }],
+    }];
+    const localData = [{
+      _id: '1l',
+      revision: '1_b',
+      remoteRevision: null,
+      data: {'some': 'other data'},
+    }];
+    const data = merger.merge(remoteData, localData);
+    expect(data.remoteData).to.eql([{
+      _id: '1r',
+      revision: '1_a',
+      revisions: [{
+        revision: '1_a',
+        data: {'hello': 'world'},
+      }],
+    }, {
+      _id: '1l',
+      revision: '1_b',
+      revisions: [{
+        revision: '1_b',
+        data: {'some': 'other data'},
+      }],
+    }]);
+    expect(data.localData).to.eql([{
+      _id: '1r',
+      revision: '1_a',
+      remoteRevision: '1_a',
+      data: {'hello': 'world'},
+    }, {
+      _id: '1l',
+      revision: '1_b',
+      remoteRevision: '1_b',
+      data: {'some': 'other data'},
+    }]);
+  });
+
+  it('should merge difficult object states', () => {
+    const remoteData = [{
+      _id: '1r',
+      revision: '1_a',
+      revisions: [{
+        revision: '1_a',
+        data: {'hello': 'world'},
+      }],
+    }, {
+      _id: '2r',
+      revision: '1_aa',
+      revisions: [{
+        revision: '1_aa',
+        data: {'hello': 'world'},
+      }],
+    }, {
+      _id: '3r',
+      revision: '1_aaa_new',
+      revisions: [{
+        revision: '1_aaa',
+        data: {'hello': 'world'},
+      }, {
+        revision: '1_aaa_new',
+        data: {'hello': 'merge'},
+      }],
+    }];
+    const localData = [{
+      _id: '1l',
+      revision: '1_b',
+      remoteRevision: null,
+      data: {'some': 'other data'},
+    }, {
+      _id: '2r',
+      revision: '1_aab',
+      remoteRevision: '1_aa',
+      data: {'hello': 'new world'},
+    }, {
+      _id: '3r',
+      revision: '1_aaab',
+      remoteRevision: '1_aaa',
+      data: {'hello': 'difficult merge'},
+    }, {
+      _id: '4l',
+      revision: '1_b4',
+      remoteRevision: '1_b4',
+      data: {'some': 'other data'},
+    }];
+    const data = merger.merge(remoteData, localData);
+    expect(data.localData).to.have.length(6);
+    expect(data.remoteData).to.have.length(6);
   });
 });
--- TainaDesktop/app/scripts/sync/index.js
+++ TainaDesktop/app/scripts/sync/index.js
@@ -1,6 +1,5 @@
 import PromiseA from 'bluebird';
 import logger from 'winston';
-import _ from 'lodash';
 
 /**
  * create Synch object
@@ -10,15 +9,15 @@ import _ from 'lodash';
  * @param  {function} Random.uuid
  * @return {Synch}
  */
-module.exports.create = function(db, syncServices, Random) {
+module.exports.create = (db, syncServices, Merger) => {
   const Sync = {};
 
-  Sync.runOne = function(syncService) {
+  Sync.runOne = (syncService) => {
     const loadRemoteData = syncService.loadData();
     const loadLocalData = db.getAllNotes();
 
     return PromiseA.join(loadRemoteData, loadLocalData, (remoteData, localData) => {
-      const merged = Sync.merge(remoteData, localData);
+      const merged = Merger.merge(remoteData, localData);
       const bulkUpdating = db.bulkUpdate(merged.localData);
       const remoteBulkSaving = syncService.bulkSave(merged.remoteData);
       return PromiseA.all([bulkUpdating, remoteBulkSaving]);
@@ -27,98 +26,10 @@ module.exports.create = function(db, syncServices, Random) {
     });
   };
 
-  Sync.run = function() {
+  Sync.run = () => {
     return PromiseA
       .map(syncServices, service => Sync.runOne(service))
       .then(() => logger.info('Synchronization was completed'));
-  };
-
-  /**
-   * @typedef  {Object} RemoteData
-   * @property {String} _id - in format 'f39c6cb6-a271-4350-bb13-20450da27827'
-   * @property {String} revision - current revision in format '1_f39c6cb6-a271-4350-bb13-20450da27827'
-   * @property {Object[]} revisions - list of all revisions
-   * @property {String} revisions[]._revision - is revision of this object
-   * @property {Object} revisions[].data - payload
-   */
-
-  /**
-   * @typedef  {Object} LocalData
-   * @property {String} _id - in format 'f39c6cb6-a271-4350-bb13-20450da27827'
-   * @property {String} remoteRevision - last saved remote revision
-   * @property {String} revision - current revision in format '1_f39c6cb6-a271-4350-bb13-20450da27827'
-   * @property {Object} data - payload
-   */
-
-  /**
-   * @method merge
-   * @description merge remote and local data. As result produce new state for remote and local data.
-   * @param  {RemoteData[]} remoteData
-   * @param  {LocalData[]} localData
-   * @return {{remoteData: RemoteData[], localData: LocalData[]}}
-   */
-  Sync.merge = function(remoteData, localData) {
-    const indexedRemoteData = _.indexBy(remoteData, '_id');
-    // if remote and local revisions are different so object was changed
-    const localChanges = _.filter(localData, doc => doc.remoteRevision !== doc.revision);
-    const localUnchanged = _.filter(localData, doc => doc.remoteRevision === doc.revision);
-
-    _.forEach(localUnchanged, doc => {
-      const id = doc._id;
-      if (!indexedRemoteData[id]) {
-        indexedRemoteData[id] = {
-          _id: id,
-          revision: doc.revision,
-          revisions: [{
-            revision: doc.revision,
-            data: doc.data,
-          }],
-        };
-      }
-    });
-
-    _.forEach(localChanges, doc => {
-      if (indexedRemoteData[doc._id] && indexedRemoteData[doc._id].revision === doc.remoteRevision) {
-        // remote data has no changes, so just add new changes
-        indexedRemoteData[doc._id].revisions.push({
-          revision: doc.revision,
-          data: doc.data,
-        });
-        indexedRemoteData[doc._id].revision = doc.revision;
-      } else {
-        // remote data has been changed too, so save local changes as new data
-        let id = Random.uuid();
-        if (_.isNull(doc.remoteRevision)) {
-          // local data is new
-          id = doc._id;
-        }
-        indexedRemoteData[id] = {
-          _id: id,
-          revision: doc.revision,
-          revisions: [{
-            revision: doc.revision,
-            data: doc.data,
-          }],
-        };
-      }
-    });
-
-    const newLocalData = [];
-    const newRemoteData = [];
-    _.forEach(indexedRemoteData, doc => {
-      newRemoteData.push(doc);
-      newLocalData.push({
-        _id: doc._id,
-        remoteRevision: doc.revision,
-        revision: doc.revision,
-        data: doc.revisions[doc.revisions.length - 1].data,
-      });
-    });
-
-    return {
-      localData: newLocalData,
-      remoteData: newRemoteData,
-    };
   };
 
   return Sync;
