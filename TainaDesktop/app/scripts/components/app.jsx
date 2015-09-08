f16a308a83da6836e9b1b6ea4481be427855c727
Ilya Siganov <ilya.blan4@gmail.com>
2015-09-08 17:39:35 +0600

    wrap components into global builder closure

--- TainaDesktop/app/scripts/components/app.jsx
+++ TainaDesktop/app/scripts/components/app.jsx
@@ -1,29 +1,34 @@
 import React from 'react';
 import Router from 'react-router';
-import Login from './login.jsx';
+import LoginCreator from './login.jsx';
 const Route = Router.Route;
 const RouteHandler = Router.RouteHandler;
 const DefaultRoute = Router.DefaultRoute;
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>App</h1>
-        <RouteHandler/>
-      </div>
-    );
+module.exports.create = (taina) => {
+  const Login = LoginCreator.create(taina);
+
+  class App extends React.Component {
+    render() {
+      return (
+        <div>
+          <h1>App</h1>
+          <RouteHandler/>
+        </div>
+      );
+    }
   }
-}
 
-const routes = (
-  <Route handler={App}>
-    <Route path="login" handler={Login}/>
-    <DefaultRoute handler={Login}/>
-  </Route>
-);
+  const routes = (
+    <Route handler={App}>
+      <Route path="login" handler={Login}/>
+      <DefaultRoute handler={Login}/>
+    </Route>
+  );
 
 
-Router.run(routes, Router.HashLocation, (Root) => {
-  React.render(<Root/>, document.getElementById('root'));
-});
+  Router.run(routes, Router.HashLocation, (Root) => {
+    React.render(<Root/>, document.getElementById('root'));
+  });
+
+};
